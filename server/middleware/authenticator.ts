53ef9c12efe890f51a3458e69be4a921acd9e390
rChinnnn <B10121035@yuntech.edu.tw>
2023-05-02 11:45:18 +0800

    feat(server side): init schema & adjust middleware

--- server/middleware/authenticator.ts
+++ server/middleware/authenticator.ts
@@ -11,25 +11,27 @@ export default defineEventHandler(async (event) => {
     const accessToken = getRequestHeader(event, 'Authorization')
     // const accessToken = await sign({uid:"exampleUid"}, JWT_SECRET, 60*60*24*30)
     // console.log(accessToken)
-    try {
-        if(!accessToken) return {
-            success: false,
-            statusCode : 401,
-            message: 'Unauthorized : must have Authorization header'
-        }
-        const {uid, exp} = await decoder(accessToken, JWT_SECRET) as JwtPayload
-        if(Date.now() >= exp*1000) return {
-            success: false,
-            statusCode : 401,
-            message: 'Unauthorized : token expired'
-        }
-        console.log({uid})
-    } catch(err) {
-        console.log(`Through authentication middleware error : `, err)
-        return {
-            success: false,
-            statusCode : 400,
-            message: JSON.stringify(err)
+    if (url!.startsWith('/api')) {
+        try {
+            if(!accessToken) return {
+                success: false,
+                statusCode : 401,
+                message: 'Unauthorized : must have Authorization header'
+            }
+            const {uid, exp} = await decoder(accessToken, JWT_SECRET) as JwtPayload
+            if(Date.now() >= exp*1000) return {
+                success: false,
+                statusCode : 401,
+                message: 'Unauthorized : token expired'
+            }
+            console.log({uid})
+        } catch(err) {
+            console.log(`Through authentication middleware error : `, err)
+            return {
+                success: false,
+                statusCode : 400,
+                message: JSON.stringify(err)
+            }
         }
     }
     

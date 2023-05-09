05449a9150bcf3fa224609c721cc46ad0b21fd3a
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2023-05-09 13:53:58 +0800

    feat(order): order page with fake data

--- pages/order.vue
+++ pages/order.vue
@@ -8,18 +8,14 @@
         <h2
           class="cart_header mb-4 border-4 border-t-0 border-l-0 border-r-0 border-black text-3xl"
         >
-          購物車
+          確認訂單
         </h2>
 
         <div
           v-if="cart.length"
           class="cart_content border-4 border-t-0 border-l-0 border-r-0 border-black"
         >
-          <div
-            v-for="(item, index) in cart"
-            :key="item.id"
-            class="cart_item mb-4 flex items-center"
-          >
+          <div v-for="item in cart" :key="item.id" class="cart_item mb-4 flex items-center">
             <img class="mr-2 border" :src="item.img" alt="img" />
             <div class="w-2/12">
               <p class="text-center">{{ item.name }}</p>
@@ -27,60 +23,48 @@
             <div class="mr-auto">
               <p class="">NT$ {{ item.price }}</p>
             </div>
-
-            <div class="">
-              <button
-                type="button"
-                class="mr-4 rounded border bg-black p-1 text-xl text-white"
-                @click="removeCartItem(index)"
-              >
-                移除
-              </button>
-            </div>
           </div>
         </div>
 
-        <div
-          v-else
-          class="absolute top-[50%] left-[50%] w-[50%] translate-x-[-50%] translate-y-[-50%] text-center"
-        >
-          <img
-            class="ml-auto mr-auto w-4/12"
-            src="/images/remove_shopping_cart.svg"
-            alt="empty_cart"
-          />
-          <p class="mb-4 text-2xl">毫無反應，是空的購物車</p>
-          <NuxtLink to="/courses">
-            <button type="button" class="rounded border bg-black p-1 text-xl text-white">
-              尋找課程
-            </button>
-          </NuxtLink>
-        </div>
-      </main>
-      <aside class="ml-1 flex w-3/12 flex-col rounded bg-white p-5">
-        <h2
-          class="cart_header mb-4 border-4 border-t-0 border-l-0 border-r-0 border-black text-3xl"
-        >
-          總計{{ cart.length }}堂課程
-        </h2>
-
-        <div class="mb-auto">
-          <div v-if="cart.length" class="flex">
+        <div class="mt-2">
+          <div class="flex">
             <span class="mr-auto">小計</span>
             <span>NT$ {{ getTotal }}</span>
           </div>
 
           <div class="mt-4 text-right text-3xl font-bold">NT$ {{ getTotal }}</div>
         </div>
+      </main>
+      <aside class="ml-1 flex w-3/12 flex-col rounded bg-white p-5">
+        <h2
+          class="cart_header mb-4 border-4 border-t-0 border-l-0 border-r-0 border-black text-3xl"
+        >
+          購買人資訊
+        </h2>
 
-        <NuxtLink to="/order">
+        <p>名稱: {{ store.userProfile.username }}</p>
+
+        <p class="mb-auto">信箱: {{ store.userProfile.email }}</p>
+
+        <!-- 接後端去藍新金流 -->
+        <button
+          :class="[cart.length ? 'bg-black' : 'bg-slate-200']"
+          class="mb-2 w-full rounded border p-1 text-xl text-white"
+          type="button"
+          :disabled="!cart.length"
+        >
+          繼續
+        </button>
+
+        <!-- 回購物車 -->
+        <NuxtLink to="/cart">
           <button
-            :class="[cart.length ? 'bg-black' : 'bg-slate-200']"
+            :class="[cart.length ? 'bg-slate-400' : 'bg-slate-200']"
             class="w-full rounded border p-1 text-xl text-white"
             type="button"
             :disabled="!cart.length"
           >
-            前往結帳
+            取消
           </button>
         </NuxtLink>
       </aside>
@@ -92,9 +76,12 @@
 import { reactive, onMounted, computed } from 'vue'
 // import { storeToRefs } from 'pinia'
 // import coursesStore from '~/stores/courses'
+import userUser from '~/stores/useUser'
 
 // const { $api } = useNuxtApp()
 // let { cart } = storeToRefs(coursesStore())
+
+const store = userUser()
 
 // fake cart
 const cart = reactive([
@@ -122,9 +109,10 @@ const getTotal = computed(() => {
   return cart.reduce((accuulator, currentItem) => accuulator + currentItem.price, 0)
 })
 
-const removeCartItem = (index: number) => {
-  cart.splice(index, 1)
-}
+// 結帳
+// const checkOut = (index: number) => {
+
+// }
 
 onMounted(async () => {
   // const cartData = await $api.course.getCart()

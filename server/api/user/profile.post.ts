74995c0168e8a8f2a9a4f81876b0b3a3c5b3f777
rChinnnn <B10121035@yuntech.edu.tw>
2023-05-16 14:35:06 +0800

    feat(backend upload): testing image upload with multer and firebase successful

--- server/api/user/profile.post.ts
+++ server/api/user/profile.post.ts
@@ -1,3 +1,4 @@
+import { promisify } from 'util'
 import Joi from 'joi'
 import multer from 'multer'
 import { callNodeListener } from 'h3'
@@ -14,27 +15,41 @@ const upload = multer({
     if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
       cb(null, true)
     } else {
-      cb(new Error('檔案格式錯誤，僅限上傳 jpg、jpeg 與 png 格式。'))
+      cb(new Error('檔案格式錯誤，僅限上傳 jpg、jpeg 與 png 格式，且大小限制 2MB 以下。'))
     }
   }
 })
 
 export default defineEventHandler(async (event) => {
-  const profileSchema = Joi.object({
-    username: Joi.string().allow('').max(30),
-    expertise: Joi.string().allow('').max(50),
-    interests: Joi.string().allow('').max(50),
-    about: Joi.string().allow('').max(300),
-    avatar: Joi.string().allow('').uri(),
-    facebookLink: Joi.string().allow('').uri(),
-    youtubeLink: Joi.string().allow('').uri(),
-    githubLink: Joi.string().allow('').uri(),
-    socialLink: Joi.string().allow('').uri()
-  })
-  const body = await readBody(event)
+  interface UpdateObj {
+    username?: string
+    expertise?: string
+    interests?: string
+    about?: string
+    avatar?: string
+    facebookLink?: string
+    youtubeLink?: string
+    githubLink?: string
+    socialLink?: string
+  }
+  const updateObj: UpdateObj = {}
+  let body: UpdateObj = {}
+  let fileUrl = ''
+
   const contenType = await getHeaders(event)['content-type']
   try {
     if (contenType === 'application/json') {
+      body = await readBody(event)
+      const profileSchema = Joi.object({
+        username: Joi.string().allow('').max(30),
+        expertise: Joi.string().allow('').max(50),
+        interests: Joi.string().allow('').max(50),
+        about: Joi.string().allow('').max(300),
+        facebookLink: Joi.string().allow('').uri(),
+        youtubeLink: Joi.string().allow('').uri(),
+        githubLink: Joi.string().allow('').uri(),
+        socialLink: Joi.string().allow('').uri()
+      })
       const { error } = profileSchema.validate(body, { abortEarly: true })
       if (error)
         throw new Error(
@@ -47,29 +62,30 @@ export default defineEventHandler(async (event) => {
     } else if (contenType?.includes('multipart/form-data')) {
       // @ts-expect-error: Nuxt 3
       await callNodeListener(upload.single('image'), event.node.req, event.node.res)
+      // @ts-expect-error: multer
+      const file = event.node.req.file
 
-      const readFile = await readMultipartFormData(event)
       const bucket = firebaseAdmin.storage().bucket()
-      const file = readFile![0]
-      const blob = bucket.file(`images/${v4()}.${file!.filename!.split('.').pop()}`)
+      const blob = bucket.file(`images/user/profile/${v4()}.${file.originalname.split('.').pop()}`)
       const blobStream = blob.createWriteStream()
 
-      blobStream.on('finish', () => {
-        blob.getSignedUrl({ action: 'read', expires: '12-31-2500' }, (fileUrl) => {
-          return {
-            success: true,
-            statusCode: 200,
-            fileUrl
-          }
+      const fileUrlPromise = new Promise((resolve, _reject) => {
+        blobStream.on('finish', async () => {
+          const getSignedUrl = promisify(blob.getSignedUrl).bind(blob)
+          const fileUrl = await getSignedUrl({ action: 'read', expires: '12-31-2500' })
+          resolve(fileUrl)
         })
       })
       blobStream.on('error', () => {
         return createError({
           statusCode: 500,
-          message: '上傳圖像失敗'
+          message: '更新 user 頭像失敗'
         })
       })
-      blobStream.end(file.data)
+      blobStream.end(file.buffer)
+
+      fileUrl = (await fileUrlPromise) as string
+      updateObj.avatar = fileUrl
     }
 
     const {
@@ -77,33 +93,16 @@ export default defineEventHandler(async (event) => {
       expertise,
       interests,
       about,
-      avatar,
       facebookLink,
       youtubeLink,
       githubLink,
       socialLink
     } = body
 
-    interface UpdateObj {
-      username?: string
-      expertise?: string
-      interests?: string
-      about?: string
-      avatar?: string
-      facebookLink?: string
-      youtubeLink?: string
-      githubLink?: string
-      socialLink?: string
-    }
-
-    const updateObj: UpdateObj = {}
-    // Object.prototype.hasOwnProperty.call(body, 'username')
-
     if (Object.prototype.hasOwnProperty.call(body, 'username')) updateObj.username = username
     if (Object.prototype.hasOwnProperty.call(body, 'expertise')) updateObj.expertise = expertise
     if (Object.prototype.hasOwnProperty.call(body, 'interests')) updateObj.interests = interests
     if (Object.prototype.hasOwnProperty.call(body, 'about')) updateObj.about = about
-    if (Object.prototype.hasOwnProperty.call(body, 'avatar')) updateObj.avatar = avatar
     if (Object.prototype.hasOwnProperty.call(body, 'facebookLink'))
       updateObj.facebookLink = facebookLink
     if (Object.prototype.hasOwnProperty.call(body, 'youtubeLink'))
@@ -131,9 +130,10 @@ export default defineEventHandler(async (event) => {
       })
     }
   } catch (error: any) {
+    const message = error.message === 'File too large' ? '檔案大小超過限制，僅限 2MB 以下。' : error.message
     return createError({
       statusCode: error.statusCode ? error.statusCode : 400,
-      message: error.message
+      message: message
     })
   }
 })

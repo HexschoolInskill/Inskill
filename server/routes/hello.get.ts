10f523fabcf5775f7c8a0e94b3ffe9a40dd1d83b
sen <senxsen@gmail.com>
2023-05-20 21:27:57 +0800

    feat(youtubeapi.ts mediaserver.ts): finish testing youtubapi with mediaserver api

--- server/routes/hello.get.ts
+++ server/routes/hello.get.ts
@@ -9,32 +9,43 @@
  */
 
 import Joi from 'joi'
+// import {getWebRTCAppList, updateWebRTCAppEndPointList} from '../services/mediaServer'
+import {createLive, startLive, endLive} from '../services/youtubeApi'
 export default defineEventHandler(async (event) => {
-  const parametersSchema = Joi.object({
-    symbol: Joi.string(),
-    limit: Joi.number().max(1000).default(500)
-  })
-  const query = getQuery(event)
+  // const parametersSchema = Joi.object({
+  //   symbol: Joi.string(),
+  //   limit: Joi.number().max(1000).default(500)
+  // })
+  // const query = getQuery(event)
   try {
-    const { error, value } = parametersSchema.validate(query, { abortEarly: true })
-    if (error)
-      throw new Error(
-        error.details
-          .map((e) => {
-            return e.message
-          })
-          .join(', ')
-      )
-    const response = await $fetch(`/exchangeInfo`, {
-      method: 'GET',
-      baseURL: `https://fapi.binance.com/fapi/v1`
-    })
+    // const { error, value } = parametersSchema.validate(query, { abortEarly: true })
+    // if (error)
+    //   throw new Error(
+    //     error.details
+    //       .map((e) => {
+    //         return e.message
+    //       })
+    //       .join(', ')
+    //   )
+    // const response = await $fetch(`/exchangeInfo`, {
+    //   method: 'GET',
+    //   baseURL: `https://fapi.binance.com/fapi/v1`
+    // })
+    // return {
+    //   success: true,
+    //   statusCode: 200,
+    //   message: '測試 [GET] /hello with Joi',
+    //   value,
+    //   response
+    // }
+    // const response = await updateWebRTCAppEndPointList("9K6nIH339mtV168412138054", "rtmp://a.rtmp.youtube.com/live2/zbkg-3m3y-wvx6-p3kw-f6fv-test")
+    const response = await createLive()
+    // const response = await startLive("7y0MA5WSVyg")
+    // const response = await endLive("7y0MA5WSVyg")
     return {
       success: true,
       statusCode: 200,
-      message: '測試 [GET] /hello with Joi',
-      value,
-      response
+      data : response
     }
   } catch (error: any) {
     return {

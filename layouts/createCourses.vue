aabab0b895721d82557dd479ebbb8ab66e68e9d1
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2023-05-23 17:23:03 +0800

    feat(partner/courses): replace tiptap with vue-quill editor
    refine create course page style and layout

--- layouts/createCourses.vue
+++ layouts/createCourses.vue
@@ -1,23 +1,23 @@
 <template>
-  <slot name="header"></slot>
+  <!-- <slot name="header"></slot> -->
 
-  <div class="mt-[15vh] flex">
-    <aside>
-      <div>課程名稱</div>
-      <ul>
-        <li>
-          <NuxtLink></NuxtLink>
+  <div class="flex">
+    <aside class="w-1/12 whitespace-nowrap p-4 text-white">
+      <div class="p-5 text-center">課程名稱</div>
+      <ul class="py-4">
+        <li class="mb-4">
+          <NuxtLink>課程資訊</NuxtLink>
         </li>
-        <li>
-          <NuxtLink></NuxtLink>
+        <li class="mb-4">
+          <NuxtLink>章節課程</NuxtLink>
         </li>
-        <li>
-          <NuxtLink></NuxtLink>
+        <li class="mb-4">
+          <NuxtLink>價格</NuxtLink>
         </li>
       </ul>
     </aside>
 
-    <main>
+    <main class="w-11/12">
       <slot />
     </main>
   </div>

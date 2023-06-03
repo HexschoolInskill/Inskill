59ccd2e1462a3d2a27b6dfb43ea9379c069b70f6
rChinnnn <b10121035@yuntech.edu.tw>
2023-06-04 02:48:24 +0800

    feat(api): confirm cart, collect, my(Live)Courses API

--- server/api/courses/collect.get.ts
+++ server/api/courses/collect.get.ts
@@ -1,13 +1,104 @@
 // 讀取 收藏課程
 
-// import models, { Course, User, LiveCourse } from '../../model/schema'
+import models from '../../model/schema'
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   try {
-    const { userInfo } = event.context.auth
+    const userID = event.context.auth.userID
+    const collectCourses = await models.User.aggregate([
+      { $match: { $expr: { $eq: ['$_id', { $toObjectId: userID }] } } },
+      { $unwind: '$collectCourses' },
+      { $match: { 'collectCourses.courseType': 'Course' } },
+      {
+        $lookup: {
+          from: 'courses',
+          localField: 'collectCourses.courseId',
+          foreignField: '_id',
+          as: 'course'
+        }
+      },
+      { $unwind: '$course' },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'course.teacherId',
+          foreignField: '_id',
+          as: 'teacher'
+        }
+      },
+      { $unwind: '$teacher' },
+      {
+        $group: {
+          _id: '$collectCourses.courseType',
+          courseType: { $first: '$collectCourses.courseType' },
+          courses: {
+            $push: {
+              _id: '$collectCourses.courseId',
+              title: '$course.title',
+              description: '$course.description',
+              teacherId: '$course.teacherId',
+              teacherName: '$teacher.username'
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          courseType: 1,
+          courses: 1
+        }
+      }
+    ])
+    const collectLiveCourses = await models.User.aggregate([
+      { $match: { $expr: { $eq: ['$_id', { $toObjectId: userID }] } } },
+      { $unwind: '$collectCourses' },
+      { $match: { 'collectCourses.courseType': 'LiveCourse' } },
+      {
+        $lookup: {
+          from: 'livecourses',
+          localField: 'collectCourses.courseId',
+          foreignField: '_id',
+          as: 'course'
+        }
+      },
+      { $unwind: '$course' },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'course.teacherId',
+          foreignField: '_id',
+          as: 'teacher'
+        }
+      },
+      { $unwind: '$teacher' },
+      {
+        $group: {
+          _id: '$collectCourses.courseType',
+          courseType: { $first: '$collectCourses.courseType' },
+          courses: {
+            $push: {
+              _id: '$collectCourses.courseId',
+              title: '$course.title',
+              description: '$course.description',
+              teacherId: '$course.teacherId',
+              teacherName: '$teacher.username'
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          courseType: 1,
+          courses: 1
+        }
+      }
+    ])
+    const collect = collectCourses.concat(collectLiveCourses)
     return {
       success: true,
-      collect: userInfo?.collect || []
+      collect
     }
   } catch (error: any) {
     return {

a7f21646399704a519ed37f7bf7fff4fede917b2
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2023-06-04 13:53:49 +0800

    feat(user/courses/[id].vue): show question and reply of the lession

---
+++ components/in-content-review.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="avatar mr-4 w-1/6">
+    <!-- <img src="" alt="avatar" /> -->
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 32 32"
+    >
+      <path
+        d="M16 8a5 5 0 1 0 5 5a5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3a3.003 3.003 0 0 1-3 3z"
+        fill="currentColor"
+      ></path>
+      <path
+        d="M16 2a14 14 0 1 0 14 14A14.016 14.016 0 0 0 16 2zm-6 24.377V25a3.003 3.003 0 0 1 3-3h6a3.003 3.003 0 0 1 3 3v1.377a11.899 11.899 0 0 1-12 0zm13.992-1.451A5.002 5.002 0 0 0 19 20h-6a5.002 5.002 0 0 0-4.992 4.926a12 12 0 1 1 15.985 0z"
+        fill="currentColor"
+      ></path>
+    </svg>
+    <p class="text-center">{{ props.review.userId }}</p>
+  </div>
+
+  <div class="w-full">
+    <in-content-star :stars="props.review.rating"></in-content-star>
+
+    <p>{{ props.review.comment }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  review: {
+    type: null,
+    default: []
+  }
+})
+</script>
--- pages/user/courses/[id].vue
+++ pages/user/courses/[id].vue
@@ -21,7 +21,7 @@
           <li
             v-for="(chapter, index) in currentCourse.chapters"
             :key="chapter.title"
-            class="my-2 rounded-md border px-4"
+            class="my-2 cursor-pointer rounded-md border px-4"
             :class="{ 'bg-white text-black': expandChapter === index }"
             @click="expandChapterController(index)"
           >
@@ -118,7 +118,7 @@
                 </div>
 
                 <div class="ml-4 w-full">
-                  <h3 class="mb-2">講師名稱</h3>
+                  <h2 class="mb-2 text-xl font-bold">講師名稱</h2>
                   <p>
                     冰而雲黃游抱哪奶校，雨手嗎回小鼻常禾草民「目雞停苦發訴人比」不現今美白寫即世汗根又麼兒候面奶新昌月：女兩後念，兌這條同給圓嗎喜出布。
                     巴目包辛正幼戊子來唱都間詞月兩卜貝乙詞：尺空字王音步動爬亭合開放生。
@@ -134,23 +134,7 @@
                 <h1 class="mx-8 mt-4 text-4xl font-bold">{{ sumUpReviews }}</h1>
 
                 <div>
-                  <div class="star mb-2 flex">
-                    <div v-for="star in Math.floor(sumUpReviews)" :key="star" class="w-10">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        xmlns:xlink="http://www.w3.org/1999/xlink"
-                        viewBox="0 0 16 16"
-                      >
-                        <g fill="none">
-                          <path
-                            d="M7.194 2.101a.9.9 0 0 1 1.614 0l1.521 3.082l3.401.495a.9.9 0 0 1 .5 1.535l-2.462 2.399l.581 3.387a.9.9 0 0 1-1.306.949l-3.042-1.6l-3.042 1.6a.9.9 0 0 1-1.306-.949l.58-3.387l-2.46-2.4a.9.9 0 0 1 .499-1.534l3.4-.495l1.522-3.082z"
-                            fill="#FFC107"
-                          ></path>
-                        </g>
-                      </svg>
-                    </div>
-                  </div>
-
+                  <in-content-star :stars="Math.floor(sumUpReviews)"></in-content-star>
                   <p>{{ currentCourse.reviews.length }} 則評價</p>
                 </div>
               </div>
@@ -160,49 +144,7 @@
                 :key="index"
                 class="my-4 flex rounded-xl border p-4"
               >
-                <div class="avatar mr-4 w-1/6">
-                  <!-- <img src="" alt="avatar" /> -->
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    viewBox="0 0 32 32"
-                  >
-                    <path
-                      d="M16 8a5 5 0 1 0 5 5a5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3a3.003 3.003 0 0 1-3 3z"
-                      fill="currentColor"
-                    ></path>
-                    <path
-                      d="M16 2a14 14 0 1 0 14 14A14.016 14.016 0 0 0 16 2zm-6 24.377V25a3.003 3.003 0 0 1 3-3h6a3.003 3.003 0 0 1 3 3v1.377a11.899 11.899 0 0 1-12 0zm13.992-1.451A5.002 5.002 0 0 0 19 20h-6a5.002 5.002 0 0 0-4.992 4.926a12 12 0 1 1 15.985 0z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
-                  <p class="text-center">{{ currentCourse.reviews[i - 1].userId }}</p>
-                </div>
-
-                <div class="w-full">
-                  <div class="star mb-2 flex">
-                    <div
-                      v-for="star in currentCourse.reviews[i - 1].rating"
-                      :key="star"
-                      class="w-10"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        xmlns:xlink="http://www.w3.org/1999/xlink"
-                        viewBox="0 0 16 16"
-                      >
-                        <g fill="none">
-                          <path
-                            d="M7.194 2.101a.9.9 0 0 1 1.614 0l1.521 3.082l3.401.495a.9.9 0 0 1 .5 1.535l-2.462 2.399l.581 3.387a.9.9 0 0 1-1.306.949l-3.042-1.6l-3.042 1.6a.9.9 0 0 1-1.306-.949l.58-3.387l-2.46-2.4a.9.9 0 0 1 .499-1.534l3.4-.495l1.522-3.082z"
-                            fill="#FFC107"
-                          ></path>
-                        </g>
-                      </svg>
-                    </div>
-                  </div>
-
-                  <p>{{ currentCourse.reviews[i - 1].comment }}</p>
-                </div>
+                <in-content-review :review="currentCourse.reviews[i - 1]"></in-content-review>
               </div>
 
               <div v-if="currentCourse.reviews.length > showReviews" class="mb-8 mt-8 text-center">
@@ -223,11 +165,11 @@
                 <label for="new_question text-2xl">新增提問</label>
                 <textarea
                   id="new_question"
-                  class="mt-4 rounded-lg bg-[#DEE2E6] p-2 text-black"
                   v-model="newQuestion"
+                  class="mt-4 rounded-lg bg-[#DEE2E6] p-2 text-black"
                   name="new_question"
                   cols="30"
-                  rows="10"
+                  rows="5"
                   placeholder="喜歡這堂課嗎? 購買課程後可以和大家已起討論囉!"
                 >
                 </textarea>
@@ -242,33 +184,77 @@
                 </button>
               </div>
 
-              <h2 class="text-2xl font-bold">
+              <h2 class="mb-4 text-2xl font-bold">
                 全部問答 ({{ currenChapterLession.question.length }})
               </h2>
 
-              <div v-if="currenChapterLession.question.length" class="flex rounded-lg border">
-                <img src="" alt="avatar" />
-
-                <div>
-                  <h2>使用者名稱</h2>
-                  <p>問答內容</p>
-
-                  <div class="flex">
-                    <button type="button">回復</button>
-                    <button type="button">共 0 則</button>
-                  </div>
-                </div>
+              <div
+                v-for="(question, index) in currenChapterLession.question"
+                :key="question.comment"
+                class="rounded-lg border p-8"
+              >
+                <in-content-question
+                  :question="question"
+                  @addReply="($value) => addingReply($value, index)"
+                ></in-content-question>
               </div>
 
               <!-- <button type="button">查看更多</button> -->
             </div>
           </div>
 
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+          <ul class="h-full w-1/12 rounded border border-[#6C757D]">
+            <li class="border-bottom py-2">
+              <svg
+                class="mx-auto w-3/6"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 16 16"
+              >
+                <g fill="none">
+                  <path
+                    d="M1 4.5A2.5 2.5 0 0 1 3.5 2h9A2.5 2.5 0 0 1 15 4.5v5a2.5 2.5 0 0 1-2.5 2.5H8.688l-3.063 2.68A.98.98 0 0 1 4 13.942V12h-.5A2.5 2.5 0 0 1 1 9.5v-5zM3.5 3A1.5 1.5 0 0 0 2 4.5v5A1.5 1.5 0 0 0 3.5 11H5v2.898L8.312 11H12.5A1.5 1.5 0 0 0 14 9.5v-5A1.5 1.5 0 0 0 12.5 3h-9z"
+                    fill="currentColor"
+                  ></path>
+                </g>
+              </svg>
+            </li>
+            <li class="border-bottom py-2">
+              <svg
+                class="mx-auto w-3/6"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M15 3l2.3 2.3l-2.89 2.87l1.42 1.42L18.7 6.7L21 9V3h-6zM3 9l2.3-2.3l2.87 2.89l1.42-1.42L6.7 5.3L9 3H3v6zm6 12l-2.3-2.3l2.89-2.87l-1.42-1.42L5.3 17.3L3 15v6h6zm12-6l-2.3 2.3l-2.87-2.89l-1.42 1.42l2.89 2.87L15 21h6v-6z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </li>
+            <li class="border-bottom py-2">
+              <svg
+                class="mx-auto w-3/6"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 24 24"
+              >
+                <path d="M15.41 7.41L14 6l-6 6l6 6l1.41-1.41L10.83 12z" fill="currentColor"></path>
+              </svg>
+            </li>
+            <li class="py-2">
+              <svg
+                class="mx-auto w-3/6"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M10.02 6L8.61 7.41L13.19 12l-4.58 4.59L10.02 18l6-6l-6-6z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </li>
           </ul>
         </div>
       </main>
@@ -329,7 +315,20 @@ const currentCourse = {
               sort: 0
             }
           ],
-          question: []
+          question: [
+            {
+              userId: 'u001',
+              comment:
+                '村喜夏蝶毛胡星同？裏次己、世我休但停候包息唱色也着法采意方，掃收重千房背着要歌位游老化杯生，掃錯哭兆習他，杯香車過欠由吧加給送快、字回不入雞彩雄瓜母完少枝外弓汁幸反！且少斥土自物都真現浪玉再追瓜春尼。',
+              reply: [
+                {
+                  userId: 'u002',
+                  comment:
+                    '歌爪加弟步，對重得錯根乙相奶去星圓、干老天雲植少經心。久乍或說美共，愛記棵看松，原眼即裏愛片至穴。太手但帽衣朱馬昔。'
+                }
+              ]
+            }
+          ]
         },
         {
           _id: '00001',
@@ -423,6 +422,14 @@ const getMoreReviews = () => {
   } else {
     showReviews.value = showReviews.value + 3
   }
+}
+
+// 新增回復
+const addingReply = (value: string, index: number) => {
+  currenChapterLession.value.question[index].reply.push({
+    userId: '',
+    comment: value
+  })
 }
 
 // onMounted(async () => {

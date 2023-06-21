7d8e4ff865f419a16dd271600bbcb54b89201a7c
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2023-06-21 11:09:37 +0800

    chore: save code changes

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,15 +1,13 @@
 <template>
   <in-container class="2xl:mt-[2vh]">
     <div class="flex h-auto flex-col justify-between py-5 sm:h-[62vh] sm:flex-row">
-      <main class="mr-4 w-full rounded-lg bg-[#262b2f] text-white p-5 sm:w-9/12">
-        <h2 class="cart_header border-bottom text-3xl pb-1">
-          購物車
-        </h2>
+      <main class="mr-4 w-full rounded-lg bg-[#262b2f] p-5 text-white sm:w-9/12">
+        <h2 class="cart_header border-bottom pb-1 text-3xl">購物車</h2>
 
         <div
           v-if="cart.length"
           class="cart_content max-h-[350px] pb-1"
-          :class="{ 'overflow-y-scroll border-bottom': cart.length > 3 }"
+          :class="{ 'border-bottom overflow-y-scroll': cart.length > 3 }"
         >
           <div
             v-for="(item, index) in cart"
@@ -27,7 +25,7 @@
             <div class="">
               <button
                 type="button"
-                class="mr-4 rounded border bg-black px-2 py-1 text-lg transition-base text-white hover:bg-[#262b2f]"
+                class="transition-base mr-4 rounded border bg-black px-2 py-1 text-lg text-white hover:bg-[#262b2f]"
                 @click="removeCartItem(index)"
               >
                 移除
@@ -53,12 +51,10 @@
           </NuxtLink>
         </div>
       </main>
-      <aside class="ml-1 mt-4 flex w-full flex-col rounded-lg bg-[#262b2f] text-white p-5 sm:mt-0 sm:w-3/12">
-        <h2
-          class="cart_header mb-4 border-bottom pb-1 text-3xl"
-        >
-          總計{{ cart.length }}堂課程
-        </h2>
+      <aside
+        class="ml-1 mt-4 flex w-full flex-col rounded-lg bg-[#262b2f] p-5 text-white sm:mt-0 sm:w-3/12"
+      >
+        <h2 class="cart_header border-bottom mb-4 pb-1 text-3xl">總計{{ cart.length }}堂課程</h2>
 
         <div class="mb-auto">
           <div v-if="cart.length" class="flex">
@@ -72,7 +68,7 @@
         <NuxtLink to="/checkout">
           <button
             :class="[cart.length ? 'bg-black' : 'bg-slate-200']"
-            class="mt-4 w-full rounded border p-1 text-xl text-white transition-base hover:bg-[#262b2f]"
+            class="transition-base mt-4 w-full rounded border p-1 text-xl text-white hover:bg-[#262b2f]"
             type="button"
           >
             前往結帳

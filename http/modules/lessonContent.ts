602092aa795ae3e4c05fa4d0703299ad1db426a6
karta2186810 <karta2186810@gmail.com>
2023-06-28 22:54:17 +0800

    feat(partner course): connect lesson content patch api

--- http/modules/lessonContent.ts
+++ http/modules/lessonContent.ts
@@ -11,16 +11,23 @@ export interface LessonContent {
   duration?: number
 }
 
-interface addLessonContentPayload {
+interface AddLessonContentPayload {
   contentType: LessonContentType
   lessonId: string
-  content: File | String
+  content: File | string
+}
+
+interface UpdateContentPayload {
+  contentType: LessonContentType | 'sort'
+  lessonId: string
+  lessonContentId: string
+  content: File | string | number
 }
 
 class LessonContentModule extends HttpFactory {
   private RESOURCE = '/lessonContent'
 
-  async addContent({ contentType, content, lessonId }: addLessonContentPayload) {
+  async addContent({ contentType, content, lessonId }: AddLessonContentPayload) {
     const formData = new FormData()
 
     if (contentType === 'text' || contentType === 'code') {
@@ -37,7 +44,26 @@ class LessonContentModule extends HttpFactory {
     }>(`${this.RESOURCE}/${lessonId}`, 'POST', formData)
   }
 
-  // async updateContent(contentType: LessonContentType) {}
+  async updateContent({ lessonContentId, lessonId, contentType, content }: UpdateContentPayload) {
+    const formData = new FormData()
+
+    if (contentType === 'text' || contentType === 'code') {
+      formData.append('data', JSON.stringify({ contentType, content }))
+    }
+    if ((contentType === 'pdf' || contentType === 'video') && content instanceof File) {
+      formData.append('data', JSON.stringify({ contentType }))
+      formData.append('file', content)
+    }
+    if (contentType === 'sort' && typeof content === 'number') {
+      formData.append('data', JSON.stringify({ contentType, content }))
+    }
+    return await this.call<{ updatedLessonContent: LessonContent[] }>(
+      `${this.RESOURCE}/${lessonId}?lessonContentId=${lessonContentId}`,
+      'PATCH',
+      formData
+    )
+  }
+
   async deleteContent(lessonId: string, lessonContentId: string) {
     return await this.call<{ success: boolean }>(
       `${this.RESOURCE}/${lessonId}?lessonContentId=${lessonContentId}`,
